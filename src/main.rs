692daea13c0ece431947b825699cf861601acc5e
Dylan Stocking <dstocking0823@gmail.com>
2023-10-22 13:43:06 -0400

    Initial start to input / response for rubber ducky

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use std::io::{ Write };
 mod greeting;
+mod rubber_ducky;
 fn main() {
     greeting::ducky_greeting();
-    let mut line = String::new();
-    print!("What do you seem to be stuck on?: ");
-    std::io::stdout().flush().unwrap();
-    std::io::stdin().read_line(&mut line).unwrap();
-    println!("You said -> {}", line)
+    rubber_ducky::rubber_ducky()
 }
